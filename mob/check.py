7e50eeb27821c0936f2afc7e68637d8e2483adbc
jumping <quzhengping@gmail.com>
2013-03-08 14:17:17 +0800

    add files

--- mob/check.py
+++ mob/check.py
@@ -21,12 +21,21 @@ class Check(object):
     def length(self):
         return self.headers.get('content-length',0)
 
-    def header(self, target):
+    def header(self, target, size=200):
+        source = self.content[:size]
+        if isinstance(source, unicode):
+            source = data.encode('utf-8')
+
+        stripped_tags = re.sub(r'<![^>]*>', '',source)
+        data = '\n'.join((l.rstrip() for l in stripped_tags.splitlines() if l.strip() != ''))
+
         TARGET = re.compile(target)
-        result = TARGET.match(self.content[:50])
+        result = TARGET.match(data)
+
         if result: 
             return result.pos
         else:
+            print data
             return None
 
 def main(url, keyword):
@@ -38,7 +47,9 @@ def main(url, keyword):
     print check.header(keyword)
 
 if __name__ == '__main__':
+    #url = 'http://api.wall.zhuamob.com/OfferList?device_id=aaa&mac=xxx&app_id=1b7544a62ac1050aff63b90ee7f3402c'
+    #keyword = """{"code":200,"""
     url = 'http://www.baidu.com'
-    keyword = 'STATUS OK'
+    keyword = """html,body"""
     main(url, keyword)
 
